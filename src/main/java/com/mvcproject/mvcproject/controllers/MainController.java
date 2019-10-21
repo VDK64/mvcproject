84816b5e5624b6493818b8f4e3261e0f607aec1b
d.voznyuk <dkvoznyuk@yandex.ru>
2019-10-21 16:12:07 +0400

    update index.ftl

--- src/main/java/com/mvcproject/mvcproject/controllers/MainController.java
+++ src/main/java/com/mvcproject/mvcproject/controllers/MainController.java
@@ -5,7 +5,6 @@ import com.mvcproject.mvcproject.entities.User;
 import com.mvcproject.mvcproject.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/mvcproject/mvcproject/services/UserService.java
+++ src/main/java/com/mvcproject/mvcproject/services/UserService.java
@@ -15,9 +15,6 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.Collections;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
