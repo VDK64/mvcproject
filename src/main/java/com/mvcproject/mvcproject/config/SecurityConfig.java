0d2ad8dc0279c1ba40c54f07c8ec91be311ac8ea
d.voznyuk <dkvoznyuk@yandex.ru>
2019-11-18 18:29:39 +0400

    favicon

--- src/main/java/com/mvcproject/mvcproject/config/SecurityConfig.java
+++ src/main/java/com/mvcproject/mvcproject/config/SecurityConfig.java
@@ -25,8 +25,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                     .authorizeRequests()
-                    .antMatchers("/readme.txt", "/css/*", "/register", "/login?ok", "/email/**", "/static/**").permitAll()
-                    .anyRequest().authenticated()
+                    .antMatchers("/readme.txt", "/css/*", "/register", "/login?ok", "/email/**",
+                            "/static/**", "/favicon.ico").permitAll()
+                .anyRequest().authenticated()
                 .and()
                     .formLogin().loginPage("/login").permitAll()
                 .and()
